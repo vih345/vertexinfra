2ed0cda2e98736841c0191a7c8e74e79daea345a
Programador Web <83261728+vih345@users.noreply.github.com>
2026-09-02 12:17:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+$siteName = 'VERTEX';
+$whatsappNumber = preg_replace('/\D+/', '', getenv('VERTEX_WHATSAPP') ?: '5511981471295');
+$whatsappNumber = $whatsappNumber ?: '5511981471295';
+$currentYear = date('Y');
+$templatePath = __DIR__ . DIRECTORY_SEPARATOR . 'index.html';
+
+if (!is_file($templatePath)) {
+    http_response_code(500);
+    exit('Template do site nao encontrado.');
+}
+
+ob_start();
+include $templatePath;
+$page = ob_get_clean();
+
+$page = str_replace(
+    '© 2026 VERTEX',
+    '© ' . $currentYear . ' ' . $siteName,
+    $page
+);
+$page = str_replace(
+    'https://wa.me/5511981471295',
+    'https://wa.me/' . $whatsappNumber,
+    $page
+);
+$page = str_replace(
+    "const NUMERO_WHATSAPP = '5511981471295';",
+    'const NUMERO_WHATSAPP = ' . json_encode($whatsappNumber, JSON_THROW_ON_ERROR) . ';',
+    $page
+);
+
+echo $page;
